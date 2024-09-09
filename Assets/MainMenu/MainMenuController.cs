5d4c1792f890d9d7a1960c08677065ef3cd1db10
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-09-09 14:56:33 -0400

    Made Controls button work
    controls button works now :)

--- Assets/MainMenu/MainMenuController.cs
+++ Assets/MainMenu/MainMenuController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public CanvasGroup OptionPanel;
+    public CanvasGroup ControlsPanel;
 
     public void PlayGame()
     {
@@ -15,19 +16,24 @@ public class MainMenuController : MonoBehaviour
     public void Option()
     {
         OptionPanel.alpha = 1;
+        //ControlPanel.alpha = 0;
         OptionPanel.blocksRaycasts = true;
     }
 
     public void Back()
     {
         OptionPanel.alpha = 0;
+        ControlsPanel.alpha = 0;
         OptionPanel.blocksRaycasts = false;
+        ControlsPanel.blocksRaycasts = false;
     }
 
     public void Controls()
     {
-        OptionPanel.alpha = 1;
-        OptionPanel.blocksRaycasts = true;
+        //OptionPanel.alpha = 1;
+        //OptionPanel.blocksRaycasts = true;
+        ControlsPanel.alpha = 1;
+        ControlsPanel.blocksRaycasts = true;
     }
     public void QuitGame()
     {
