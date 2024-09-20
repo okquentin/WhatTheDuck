9762a7ffcb06b0be72988ea24f5d40d5ff4c3480
Quentin <112091207+okquentin@users.noreply.github.com>
2024-09-20 03:44:55 -0400

    character creation added

--- Assets/MainMenu/MainMenuController.cs
+++ Assets/MainMenu/MainMenuController.cs
@@ -10,7 +10,7 @@ public class MainMenuController : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 
     public void Option()
